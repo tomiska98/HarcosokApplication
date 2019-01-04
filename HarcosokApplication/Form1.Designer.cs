dcae15e3f66279f52651f61df8cf868861893091
Tamás Koronczay <tomiska98@gmail.com>
2019-01-04 21:45:40 +0100

    Készen van a feladatom

---
+++ HarcosokApplication/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace HarcosokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hasznaloComboBox = new System.Windows.Forms.ComboBox();
+            this.harcosNeveTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.kepessegNeveTextBox = new System.Windows.Forms.TextBox();
+            this.harcosokListBox = new System.Windows.Forms.ListBox();
+            this.kepessegekListBox = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.leirasTextBox = new System.Windows.Forms.TextBox();
+            this.kepessegLeirasaTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Név:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Létrehozás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Használó";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Név:";
+            // 
+            // hasznaloComboBox
+            // 
+            this.hasznaloComboBox.FormattingEnabled = true;
+            this.hasznaloComboBox.Location = new System.Drawing.Point(79, 100);
+            this.hasznaloComboBox.Name = "hasznaloComboBox";
+            this.hasznaloComboBox.Size = new System.Drawing.Size(168, 21);
+            this.hasznaloComboBox.TabIndex = 5;
+            // 
+            // harcosNeveTextBox
+            // 
+            this.harcosNeveTextBox.Location = new System.Drawing.Point(79, 27);
+            this.harcosNeveTextBox.Name = "harcosNeveTextBox";
+            this.harcosNeveTextBox.Size = new System.Drawing.Size(130, 20);
+            this.harcosNeveTextBox.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Leírás";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Hozzáad";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Harcosok:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(144, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Képességei:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(291, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Képesség leírása";
+            // 
+            // kepessegNeveTextBox
+            // 
+            this.kepessegNeveTextBox.Location = new System.Drawing.Point(79, 139);
+            this.kepessegNeveTextBox.Name = "kepessegNeveTextBox";
+            this.kepessegNeveTextBox.Size = new System.Drawing.Size(168, 20);
+            this.kepessegNeveTextBox.TabIndex = 15;
+            // 
+            // harcosokListBox
+            // 
+            this.harcosokListBox.FormattingEnabled = true;
+            this.harcosokListBox.Location = new System.Drawing.Point(16, 251);
+            this.harcosokListBox.Name = "harcosokListBox";
+            this.harcosokListBox.Size = new System.Drawing.Size(96, 173);
+            this.harcosokListBox.TabIndex = 18;
+            // 
+            // kepessegekListBox
+            // 
+            this.kepessegekListBox.FormattingEnabled = true;
+            this.kepessegekListBox.Location = new System.Drawing.Point(147, 251);
+            this.kepessegekListBox.Name = "kepessegekListBox";
+            this.kepessegekListBox.Size = new System.Drawing.Size(100, 173);
+            this.kepessegekListBox.TabIndex = 19;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(511, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Módosít";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(147, 431);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Törlés";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 77);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(114, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Képesség hozzáadása";
+            // 
+            // leirasTextBox
+            // 
+            this.leirasTextBox.Location = new System.Drawing.Point(294, 93);
+            this.leirasTextBox.Multiline = true;
+            this.leirasTextBox.Name = "leirasTextBox";
+            this.leirasTextBox.Size = new System.Drawing.Size(292, 66);
+            this.leirasTextBox.TabIndex = 23;
+            // 
+            // kepessegLeirasaTextBox
+            // 
+            this.kepessegLeirasaTextBox.Location = new System.Drawing.Point(294, 251);
+            this.kepessegLeirasaTextBox.Multiline = true;
+            this.kepessegLeirasaTextBox.Name = "kepessegLeirasaTextBox";
+            this.kepessegLeirasaTextBox.Size = new System.Drawing.Size(292, 76);
+            this.kepessegLeirasaTextBox.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(19, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Harcos létrehozása";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 463);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.kepessegLeirasaTextBox);
+            this.Controls.Add(this.leirasTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.kepessegekListBox);
+            this.Controls.Add(this.harcosokListBox);
+            this.Controls.Add(this.kepessegNeveTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.harcosNeveTextBox);
+            this.Controls.Add(this.hasznaloComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox hasznaloComboBox;
+        private System.Windows.Forms.TextBox harcosNeveTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox kepessegNeveTextBox;
+        private System.Windows.Forms.ListBox harcosokListBox;
+        private System.Windows.Forms.ListBox kepessegekListBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox leirasTextBox;
+        private System.Windows.Forms.TextBox kepessegLeirasaTextBox;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ HarcosokApplication/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HarcosokApplication
+{
+    public partial class Form1 : Form
+    {
+        string connString = "Server=localhost;Uid=root;Database=cs_harcosok";
+        public MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection(connString);
+                {
+                    conn.Open();
+                    MySqlCommand SET = new MySqlCommand("Insert into harcosok(nev) values(@nev)", conn);
+                    SET.Parameters.AddWithValue("@nev", harcosNeveTextBox.Text);
+
+
+                    SET.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Van már ilyen név!");
+            }
+        }
+
+         
+    }
+}
